aa276ddec37116e8f6321a38f7ed71253c4624bc
SeanPower <seanmp22@gmail.com>
2021-11-06 14:30:31 -0400

    matchup container redesign

--- src/components/MatchupContainer/MatchupContainer.js
+++ src/components/MatchupContainer/MatchupContainer.js
@@ -8,13 +8,13 @@ function MatchupContainer() {
       <div className="matchup-container">
           <span className='matchup-card-header'>Prestige Worldwide</span>
           <MatchupCard
-            apiUrl={'https://api.sleeper.app/v1/league/652518774834044928/matchups/8'}
+            apiUrl={'https://api.sleeper.app/v1/league/652518774834044928/matchups/9'}
             heroIndex={0}
             villainIndex={1}
           />
           <span className='matchup-card-header'>Megalabowl</span>
           <MatchupCard
-            apiUrl={'https://api.sleeper.app/v1/league/740771306848673792/matchups/8'}
+            apiUrl={'https://api.sleeper.app/v1/league/740771306848673792/matchups/9'}
             heroIndex={2}
             villainIndex={6}
           />
